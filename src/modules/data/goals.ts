912911224cf2593475981aefa47deed8809aa95a
Ali Akbar <ali5alkaf5@gmail.com>
2023-09-01 16:58:17 +0700

    Vercel Config

--- src/modules/data/goals.ts
+++ src/modules/data/goals.ts
@@ -41,5 +41,138 @@ export const Goals = [
     goal_type: 'resolution',
     meta: {},
     created_at: '2019-08-17T14:15:22Z'
-  }
+  },
+  {
+    id: 'GhtHVSB12NHGBSGHHg',
+    user_id: 'GhtHVSB12NHGBSGHHg',
+    user: {
+      name: 'Fitri',
+      avatar: 'https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'
+    },
+    category: Categories[1],
+    caption: 'Lorem ipsum dolor sit amet.',
+    photos: ['https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'],
+    is_liked_by_user: false,
+    cheers_count: 20,
+    comments_count: 10,
+    visibility: 'public', // public, supporter, private
+    date_time: '2019-08-24T14:15:22Z',
+    goal_type: 'resolution',
+    meta: {},
+    created_at: '2019-08-17T14:15:22Z'
+  },
+  {
+    id: 'GhtHVSB12NHGBSGHHg',
+    user_id: 'GhtHVSB12NHGBSGHHg',
+    user: {
+      name: 'Fitri',
+      avatar: 'https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'
+    },
+    category: Categories[2],
+    caption: 'Lorem ipsum dolor sit amet.',
+    photos: ['https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'],
+    is_liked_by_user: false,
+    cheers_count: 20,
+    comments_count: 10,
+    visibility: 'public', // public, supporter, private
+    date_time: '2019-08-24T14:15:22Z',
+    goal_type: 'resolution',
+    meta: {},
+    created_at: '2019-08-17T14:15:22Z'
+  },
+  {
+    id: 'GhtHVSB12NHGBSGHHg',
+    user_id: 'GhtHVSB12NHGBSGHHg',
+    user: {
+      name: 'Fitri',
+      avatar: 'https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'
+    },
+    category: Categories[4],
+    caption: 'Lorem ipsum dolor sit amet.',
+    photos: ['https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'],
+    is_liked_by_user: false,
+    cheers_count: 20,
+    comments_count: 10,
+    visibility: 'public', // public, supporter, private
+    date_time: '2019-08-24T14:15:22Z',
+    goal_type: 'resolution',
+    meta: {},
+    created_at: '2019-08-17T14:15:22Z'
+  },
+  {
+    id: 'GhtHVSB12NHGBSGHHg',
+    user_id: 'GhtHVSB12NHGBSGHHg',
+    user: {
+      name: 'Fitri',
+      avatar: 'https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'
+    },
+    category: Categories[1],
+    caption: 'Lorem ipsum dolor sit amet.',
+    photos: ['https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'],
+    is_liked_by_user: false,
+    cheers_count: 20,
+    comments_count: 10,
+    visibility: 'public', // public, supporter, private
+    date_time: '2019-08-24T14:15:22Z',
+    goal_type: 'resolution',
+    meta: {},
+    created_at: '2019-08-17T14:15:22Z'
+  },
+  {
+    id: 'GhtHVSB12NHGBSGHHg',
+    user_id: 'GhtHVSB12NHGBSGHHg',
+    user: {
+      name: 'Fitri',
+      avatar: 'https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'
+    },
+    category: Categories[3],
+    caption: 'Lorem ipsum dolor sit amet.',
+    photos: ['https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'],
+    is_liked_by_user: false,
+    cheers_count: 20,
+    comments_count: 10,
+    visibility: 'public', // public, supporter, private
+    date_time: '2019-08-24T14:15:22Z',
+    goal_type: 'resolution',
+    meta: {},
+    created_at: '2019-08-17T14:15:22Z'
+  },
+  {
+    id: 'GhtHVSB12NHGBSGHHg',
+    user_id: 'GhtHVSB12NHGBSGHHg',
+    user: {
+      name: 'Fitri',
+      avatar: 'https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'
+    },
+    category: Categories[2],
+    caption: 'Lorem ipsum dolor sit amet.',
+    photos: ['https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'],
+    is_liked_by_user: false,
+    cheers_count: 20,
+    comments_count: 10,
+    visibility: 'public', // public, supporter, private
+    date_time: '2019-08-24T14:15:22Z',
+    goal_type: 'resolution',
+    meta: {},
+    created_at: '2019-08-17T14:15:22Z'
+  },
+  {
+    id: 'GhtHVSB12NHGBSGHHg',
+    user_id: 'GhtHVSB12NHGBSGHHg',
+    user: {
+      name: 'Fitri',
+      avatar: 'https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'
+    },
+    category: Categories[1],
+    caption: 'Lorem ipsum dolor sit amet.',
+    photos: ['https://ik.imagekit.io/at4li2svjc/PzV4gC17iYZl_HemoeHWaL'],
+    is_liked_by_user: false,
+    cheers_count: 20,
+    comments_count: 10,
+    visibility: 'public', // public, supporter, private
+    date_time: '2019-08-24T14:15:22Z',
+    goal_type: 'resolution',
+    meta: {},
+    created_at: '2019-08-17T14:15:22Z'
+  },
 ]
