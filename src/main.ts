f76bed3b61b5389438231260b73c52ff4d927d95
Ali Akbar <ali5alkaf5@gmail.com>
2024-08-26 05:18:24 +0700

    Fixed Vercel

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import MainApp from './main-app.vue'
-import router from './router'
+import router from '@/router'
 import vue3GoogleLogin from 'vue3-google-login'
 
 // unocss
--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,7 @@ const routes = [
   }
 ]
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes
 })
