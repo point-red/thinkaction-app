d3dd94cd9682833fed4eb9dbb7b5d722ddc6db0c
Ali Akbar <ali5alkaf5@gmail.com>
2024-10-22 06:40:00 +0700

    Login Fix

--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -1,10 +1,12 @@
 export const routes = [
   {
     path: '/login',
+    name: 'Login',
     component: () => import('./views/page-login.vue')
   },
   {
     path: '/register',
+    name: 'Register',
     component: () => import('./views/page-register.vue')
   }
 ]
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes as authRoutes } from '@/modules/auth/routes'
 import { routes as mainRoutes } from '@/modules/main/routes'
 import { routes as templateRoutes } from '@/modules/template/routes'
+import client from '@/lib/connection'
 
 const routes = [
   ...authRoutes,
@@ -18,6 +19,21 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched?.some((a) => ['Login', 'Register'].includes(a.name as string))) {
+    next()
+  }
+  try {
+    await client().get('/users/history?limit=1')
+  } catch (e: any) {
+    if (e.response?.status === 401) {
+      next({ name: 'Login' })
+      return
+    }
+  }
+  next()
 })
 
 export default router
